64dfbc4ddb87ef5258b813359948c6741aa8961f
edmilson-dk <droidvidaboa@gmail.com>
2021-09-28 12:15:31 -0300

    feat: created make login admin controller

---
+++ src/infra/adapters/express-adapter/index.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+import { BaseController } from "../../presentation/controllers/base-controller";
+import { HttpRequest } from "../../presentation/http/ports";
+
+export const adaptRoute = (controller: BaseController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      params: req.params,
+      query: req.query,
+      rest: { ...req },
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  };
+};

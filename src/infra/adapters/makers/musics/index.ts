1e9fb4d682fe204c0ee35921eb95e39816ac6571
edmilson-dk <droidvidaboa@gmail.com>
2021-09-29 16:52:33 -0300

    feat: created get musics controller

--- src/infra/adapters/makers/musics/index.ts
+++ src/infra/adapters/makers/musics/index.ts
@@ -1,13 +1,20 @@
 import { MusicUseCases } from "../../../../application/use-cases/music";
 import { BaseController } from "../../../presentation/controllers/base-controller";
 import { AddMusicController } from "../../../presentation/controllers/music/add-music-controller";
+import { GetMusicsController } from "../../../presentation/controllers/music/get-musics-controller";
 import { MusicRepository } from "../../../repositories/prisma/music/music-repository";
 import { MusicServices } from "../../../services/musics";
 
+const musicServices = new MusicServices();
+const musicRepository = new MusicRepository();
+const musicUseCases = new MusicUseCases(musicRepository, musicServices);
+
 export function makeAddMusicController(): BaseController {
-  const musicServices = new MusicServices();
-  const musicRepository = new MusicRepository();
-  const musicUseCases = new MusicUseCases(musicRepository, musicServices);
   const controller = new AddMusicController(musicUseCases);
+  return controller;
+}
+
+export function makeGetMusicsController(): BaseController {
+  const controller = new GetMusicsController(musicUseCases);
   return controller;
 }

623d23a07e6a3d858264fef197a692e7a9de8442
edmilson-dk <droidvidaboa@gmail.com>
2021-09-29 17:34:54 -0300

    feat: finished get musics list

--- src/infra/routes/express/musics/index.ts
+++ src/infra/routes/express/musics/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { adaptRoute } from "../../../adapters/express-adapter";
-import { makeAddMusicController } from "../../../adapters/makers/musics";
+import { makeAddMusicController, makeGetMusicsController } from "../../../adapters/makers/musics";
 import { authMiddleware } from "../middlewares/auth";
 
 export const musicRouter: Router = Router();
 
 musicRouter.post("/add", authMiddleware, adaptRoute(makeAddMusicController()));
+musicRouter.get("/list", adaptRoute(makeGetMusicsController()));

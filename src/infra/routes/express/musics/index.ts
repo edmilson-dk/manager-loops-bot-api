4601c2bda09ecbc1448211f1d6e4abf183747362
edmilson-dk <droidvidaboa@gmail.com>
2021-10-03 18:25:05 -0300

    feat: added auth middleware

--- src/infra/routes/express/musics/index.ts
+++ src/infra/routes/express/musics/index.ts
@@ -14,4 +14,4 @@ export const musicRouter: Router = Router();
 musicRouter.post("/add", authMiddleware, adaptRoute(makeAddMusicController()));
 musicRouter.get("/list", adaptRoute(makeGetMusicsController()));
 musicRouter.get("/download/:id", adaptRouteWithSendFileStream(makeDownloadMusicFileController()));
-musicRouter.delete("/drop/:id", adaptRoute(makeDropMusicController()));
+musicRouter.delete("/drop/:id", authMiddleware, adaptRoute(makeDropMusicController()));

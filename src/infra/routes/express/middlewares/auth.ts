f166b7a4f341bc5a8b71cf77d4bd910cd624572a
edmilson-dk <droidvidaboa@gmail.com>
2021-10-14 12:20:44 -0300

    feat: added new http status code

--- src/infra/routes/express/middlewares/auth.ts
+++ src/infra/routes/express/middlewares/auth.ts
@@ -6,24 +6,24 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ error: "No token provieded" });
+    return res.status(403).json({ error: "No token provieded" });
   }
 
   const parts = authHeader.split(" ");
 
   if (!(parts.length === 2)) {
-    return res.status(401).json({ error: "Token properties error" });
+    return res.status(403).json({ error: "Token properties error" });
   }
 
   const [scheme, token] = parts;
 
   if (!/^Bearer$/.test(scheme)) {
-    return res.status(401).json({ error: "Token malformatted" });
+    return res.status(403).json({ error: "Token malformatted" });
   }
 
   verifyJWT(token, (err: any, decoded: any) => {
     if (err) {
-      return res.status(401).json({ error: "Token invalid" });
+      return res.status(403).json({ error: "Token invalid" });
     }
 
     req.userId = decoded.id;

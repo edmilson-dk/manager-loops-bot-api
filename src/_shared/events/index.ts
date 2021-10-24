859dfe6f2242ea3e1acdb97de41b4e9a3e6c93d3
edmilson-dk <droidvidaboa@gmail.com>
2021-10-24 12:23:04 -0300

    feat: created manager socket on actual music playing

--- src/_shared/events/index.ts
+++ src/_shared/events/index.ts
@@ -3,5 +3,7 @@ export const SOCKET_EVENTS = {
   addedNewMusic: "addedNewMusic",
   musicIsPlaying: "music_is_playing",
   onMusicIsPlaying: "on_music_is_playing",
+  getActualMusicPlaying: "get_actual_music_playing",
+  loopMusicIsPlaying: "loop_music_is_playing",
   roomMain: "room_main",
 };
--- src/_shared/sockets/index.ts
+++ src/_shared/sockets/index.ts
@@ -1,9 +1,11 @@
 import SocketIO from "socket.io";
 import http from "http";
 import { SOCKET_EVENTS } from "../events";
+import { ManagerMemory } from "../manager-memory";
 
 export class Sockets {
   public readonly io: SocketIO.Server;
+  private readonly managerMemory = new ManagerMemory();
 
   constructor(server: http.Server) {
     this.io = new SocketIO.Server(server, {
@@ -16,14 +18,29 @@ export class Sockets {
 
   private onMusicPlaying(data: any) {
     console.log("Music is playing: ", data);
+    this.managerMemory.setActualMusic(data);
     this.io.in(SOCKET_EVENTS.roomMain).emit(SOCKET_EVENTS.onMusicIsPlaying, data);
+  }
+
+  private onGetActualMusicPlaying(data: any) {
+    const actualMusic = this.managerMemory.getActualMusic();
+
+    console.log("Get actual music playing: ", actualMusic);
+    this.io.in(SOCKET_EVENTS.roomMain).emit(SOCKET_EVENTS.onMusicIsPlaying, actualMusic);
   }
 
   private onConnection(socket: SocketIO.Socket) {
     console.log("Socket connected with id: ", socket.id);
 
     socket.join(SOCKET_EVENTS.roomMain);
+
+    // events for main room
+    socket.on(SOCKET_EVENTS.loopMusicIsPlaying, (data) => {
+      console.log("Loop music is playing: ", data);
+    });
+
     socket.on(SOCKET_EVENTS.musicIsPlaying, (data) => this.onMusicPlaying(data));
+    socket.on(SOCKET_EVENTS.getActualMusicPlaying, (data) => this.onGetActualMusicPlaying(data));
   }
 
   private onDisconnect(socket: SocketIO.Socket) {
